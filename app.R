778efe4be43aacc763585dec075c0ba171aab6be
AlexSandoval <a.sandoval0014>
2018-10-05 16:54:30 -0400

    moved the cleaning to the server

--- app.R
+++ app.R
@@ -12,15 +12,80 @@ library(htmltools)
  
 
 
-  url <- "https://phl.carto.com/api/v2/sql?q=SELECT+*+FROM+shootings" 
-  # Make the Request
-  r <- RETRY("GET", URLencode(url))
-  
-  # Extract Content
-  c <- content(r, "text")
-  
-  # Create Dataframe
-  dat <- data.frame(jsonlite::fromJSON(c)$rows)
+
+
+# A couple of fields in the shootings data were modified to help build the app. 
+# Code_2 is a cleaner field than code
+# Wound  field was cleaned and turned into broader categories
+# Race field did not include Latino status, which was a separate field. It was then cleaned to be inclusive
+# Sex was cleaned from M and F to Male and Female
+# NA's were included where there was blank data
+
+# shootings.load <- shootings %>%
+#   mutate(dc_key = as.character(dc_key),
+#          the_geom_webmercator = as.character(the_geom_webmercator),
+#          location = as.factor(location))
+
+pdf(NULL)
+
+# Define UI for application that draws a histogram
+ui <- navbarPage("Exploring Shooting Victim Data from Philadelphia", 
+                 tabPanel("Plot",
+                          sidebarLayout(
+                            sidebarPanel(
+                              # Selecting type of crime
+                              selectInput("crimeSelect",
+                                          "Type of Incident:",
+                                          choices = sort(unique(shootings.load$code)),
+                                          multiple = TRUE,
+                                          selectize = TRUE,
+                                          selected = c("Aggravated Assualt", "Robbery", "Homicide", "Hospital Cases")),
+                              
+                              # Year of Incident Slider
+                              sliderInput("yearSelect",
+                                          "Year of Incident:",
+                                          min = min(shootings.load$year, na.rm = T),
+                                          max = max(shootings.load$year, na.rm = T),
+                                          value = c(min(shootings.load$year, na.rm = T), max(shootings.load$year, na.rm = T)),
+                                          step = 1),
+                              
+                              # Check box Input for whether incident occured inside
+                              checkboxGroupInput(inputId = "IncidentInside",
+                                                 label = "Was the Incident Inside?:",
+                                                 choiceNames = list("Yes", "No"),
+                                                 choiceValues = list("1", "0")
+                              ),
+                              
+                              # Action button
+                              actionButton("reset", "Reset Filters", icon = icon("refresh"))
+                            ),
+                            
+                            # Output plot
+                            mainPanel(
+                              plotlyOutput("woundplotc"), 
+                              plotlyOutput("codeplot"),
+                              plotlyOutput("raceplot"))
+                          )),
+                 
+                 # Data Table
+                 tabPanel("Table",
+                          inputPanel(
+                            downloadButton("downloadData","Download Victim Data")
+                          ),
+                          fluidPage(DT::dataTableOutput("table"))
+                 )
+)
+
+# Define server logic
+url <- "https://phl.carto.com/api/v2/sql?q=SELECT+*+FROM+shootings" 
+# Make the Request
+r <- RETRY("GET", URLencode(url))
+
+# Extract Content
+c <- content(r, "text")
+
+# Create Dataframe
+dat <- data.frame(jsonlite::fromJSON(c)$rows)
 
 
 # Change data types
@@ -157,70 +222,6 @@ newdat <- dat %>%
 
 # Upload Philly shooting victim data from Opendataphilly
 shootings.load <- newdat
-
-# A couple of fields in the shootings data were modified to help build the app. 
-# Code_2 is a cleaner field than code
-# Wound  field was cleaned and turned into broader categories
-# Race field did not include Latino status, which was a separate field. It was then cleaned to be inclusive
-# Sex was cleaned from M and F to Male and Female
-# NA's were included where there was blank data
-
-# shootings.load <- shootings %>%
-#   mutate(dc_key = as.character(dc_key),
-#          the_geom_webmercator = as.character(the_geom_webmercator),
-#          location = as.factor(location))
-
-pdf(NULL)
-
-# Define UI for application that draws a histogram
-ui <- navbarPage("Exploring Shooting Victim Data from Philadelphia", 
-                 tabPanel("Plot",
-                          sidebarLayout(
-                            sidebarPanel(
-                              # Selecting type of crime
-                              selectInput("crimeSelect",
-                                          "Type of Incident:",
-                                          choices = sort(unique(shootings.load$code)),
-                                          multiple = TRUE,
-                                          selectize = TRUE,
-                                          selected = c("Aggravated Assualt", "Robbery", "Homicide", "Hospital Cases")),
-                              
-                              # Year of Incident Slider
-                              sliderInput("yearSelect",
-                                          "Year of Incident:",
-                                          min = min(shootings.load$year, na.rm = T),
-                                          max = max(shootings.load$year, na.rm = T),
-                                          value = c(min(shootings.load$year, na.rm = T), max(shootings.load$year, na.rm = T)),
-                                          step = 1),
-                              
-                              # Check box Input for whether incident occured inside
-                              checkboxGroupInput(inputId = "IncidentInside",
-                                                 label = "Was the Incident Inside?:",
-                                                 choiceNames = list("Yes", "No"),
-                                                 choiceValues = list("1", "0")
-                              ),
-                              
-                              # Action button
-                              actionButton("reset", "Reset Filters", icon = icon("refresh"))
-                            ),
-                            
-                            # Output plot
-                            mainPanel(
-                              plotlyOutput("woundplotc"), 
-                              plotlyOutput("codeplot"),
-                              plotlyOutput("raceplot"))
-                          )),
-                 
-                 # Data Table
-                 tabPanel("Table",
-                          inputPanel(
-                            downloadButton("downloadData","Download Victim Data")
-                          ),
-                          fluidPage(DT::dataTableOutput("table"))
-                 )
-)
-
-# Define server logic
 server <- function(input, output, session = session) {
   # Filtered shootings data
   shootInput <- reactive({
@@ -276,7 +277,7 @@ server <- function(input, output, session = session) {
   output$raceplot <- renderPlotly({
     dat <- shootInput()
     ggplotly(
-      ggplot(data = dat, na.rm = TRUE, aes(x = race, fill = race)) + 
+      ggplot(data = dat, aes(x = race, fill = race)) + 
         geom_bar (position = position_dodge(width = 0.9)) +
         xlab("Race") +
         theme(legend.title = element_blank()) +
